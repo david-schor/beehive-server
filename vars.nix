6a0418700afa6a1e85c79af7ac045feae8256d52
David Schor <18281740+david-schor@users.noreply.github.com>
2026-02-25 09:06:52 +0100

    add ssh pub

--- vars.nix
+++ vars.nix
@@ -2,5 +2,5 @@
   fullName = "David Schor";
   userName = "david-schor";
   userEmail = "18281740+david-schor@users.noreply.github.com";
-  sshPublicKey = "";
+  sshPublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICTn60vgd57G6hZPHEzn7E3q7pyMI7fiKHdm5hJgES+n beehive-server";
   }

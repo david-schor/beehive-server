d761bdf6e2c082d816138bb0a4253fd7f69c7f4b
David Schor <18281740+david-schor@users.noreply.github.com>
2026-03-21 12:04:00 +0100

    vars file import

--- flake.nix
+++ flake.nix
@@ -1,41 +1,38 @@
 {
-    description = "base os for k3s cluster homelab";
+  description = "base os for k3s cluster homelab";
 
-    inputs =  {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
-        impermanence.url = "github:nix-community/impermanence";
+    impermanence.url = "github:nix-community/impermanence";
 
-        sops-nix = {
-            url = "github:Mic92/sops-nix";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+  };
 
-    outputs = { 
-        self, 
-        nixpkgs, 
-        ... 
-    }@inputs: {
-        vars = import ./vars.nix;
+  outputs = { nixpkgs, ... }:
+  let
+    vars = import ./vars.nix;
+  in {
+    nixosConfigurations = {
+      bee-server = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit vars; };
+        modules = [
+          ./configuration.nix
+        ];
+      };
 
-        nixosConfigurations = {
-            bee-server = nixpkgs.lib.nixosSystem {
-                system = "x86_64-linux";
-                specialArgs = { inherit vars; };
-                modules = [
-                    ./configuration.nix
-                ];
-            };
-
-            bee-server-iso = nixpkgs.lib.nixosSystem {
-                system = "x86_64-linux";
-                specialArgs = { inherit vars; };
-                modules = [
-                    "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
-                    ./modules/iso.nix
-                ];
-            };
-        };
+      bee-server-iso = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit vars; };
+        modules = [
+          "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+          ./modules/iso.nix
+        ];
+      };
     };
+  };
 }

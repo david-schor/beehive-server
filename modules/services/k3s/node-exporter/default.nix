b7563ed3d710108478169960dc70fc8adc94c5cc
David Schor <18281740+david-schor@users.noreply.github.com>
2026-08-28 09:27:09 +0200

    fix hash

--- modules/services/k3s/node-exporter/default.nix
+++ modules/services/k3s/node-exporter/default.nix
@@ -4,7 +4,7 @@ let
   image = pkgs.dockerTools.pullImage {
     imageName = "docker.io/prom/node-exporter";
     imageDigest = "sha256:1b4e4438faca4dd7e001dd445d161a4a2091b0fededa84093b3a8dfeae1f1be0";
-    hash = "sha256-Us01w7MzoSLV6441UT+TqTZ7pyZubg1KpTi/qfXFQ/o=";
+    hash = "sha256-gseqDwP6Yl62R3U6xIfD9nMbgmScx1F7CBw0SyL3lXI=";
     finalImageTag = "latest";
     arch = "amd64";
   };
